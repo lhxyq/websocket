594b85ac42cf5ea6f09121560853af874f62bc5f
lhxyq <2446059046@qq.com>
2017-05-23 13:32:33 +0800

    项目初始化

---
+++ customer/src/main/java/com/lh/customer/test.java
@@ -0,0 +1,7 @@
+package com.lh.customer;
+
+/**
+ * Created by LH 2446059046@qq.com on 2017/5/23.
+ */
+public class test {
+}
---
+++ provider/src/main/java/com/lh/provider/test.java
@@ -0,0 +1,7 @@
+package com.lh.provider;
+
+/**
+ * Created by LH 2446059046@qq.com on 2017/5/23.
+ */
+public class test {
+}
